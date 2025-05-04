56442d9c5f68ce09aba216ed846153fb3063f3ea
Daniel Nery <danielnso97@gmail.com>
2025-05-03 23:16:35 -0300

    Fix track graph aspect ratio

--- web/src/app/pages/track/track-editor/track-editor.component.ts
+++ web/src/app/pages/track/track-editor/track-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, ElementRef, signal, ViewChild } from "@angular/core";
+import { Component, computed, ElementRef, HostListener, signal, ViewChild } from "@angular/core";
 
 import { TrackService } from "../../../services/track.service";
 import { BaseChartDirective } from "ng2-charts";
@@ -13,7 +13,35 @@ export class TrackEditorComponent {
   @ViewChild("container") chartContainer!: ElementRef;
   @ViewChild("childContainer") childContainer!: ElementRef;
 
-  width = signal(500);
+  width = signal(0);
+  height = signal(0);
+
+  limits = computed<[number, number, number, number]>(() => {
+    const xs = this.trackService.track().map((p) => p.x);
+    const ys = this.trackService.track().map((p) => p.y);
+
+    const [minx, maxx] = [Math.min(...xs), Math.max(...xs)];
+    const [miny, maxy] = [Math.min(...ys), Math.max(...ys)];
+
+    const [dx, dy] = [maxx - minx, maxy - miny];
+
+    const ratio = this.width() / this.height();
+    const track_ratio = dx / dy;
+
+    if (track_ratio < ratio) {
+      const dX = (this.width() * dy) / this.height();
+      const maxg = minx + dX + 5;
+      const md = (maxg - maxx) / 2;
+
+      return [minx - 5 - md, maxg - md, miny - 5, maxy + 5];
+    }
+
+    const dY = (this.height() * dx) / this.width();
+    const maxg = miny + dY + 5;
+    const md = (maxg - maxy) / 2;
+
+    return [minx - 5, maxx + 5, miny - 5 - md, maxg - md];
+  });
 
   data = computed<ChartData<"scatter", Point[]>["datasets"]>(() => {
     const points = this.trackService.track();
@@ -48,33 +76,23 @@ export class TrackEditorComponent {
     maintainAspectRatio: false,
     scales: {
       x: {
+        min: this.limits()[0],
+        max: this.limits()[1],
         grid: {
           color: "#FFFFFF30",
         },
         ticks: {
-          //@ts-ignore
-          count: (ctx) => {
-            const {
-              scale: { maxWidth },
-            } = ctx;
-
-            return maxWidth / 10;
-          },
+          stepSize: 15,
         },
       },
       y: {
+        min: this.limits()[2],
+        max: this.limits()[3],
         grid: {
           color: "#FFFFFF30",
         },
         ticks: {
-          //@ts-ignore
-          count: (ctx) => {
-            const {
-              scale: { maxHeight },
-            } = ctx;
-
-            return maxHeight / 10;
-          },
+          stepSize: 15,
         },
       },
     },
@@ -83,6 +101,13 @@ export class TrackEditorComponent {
   constructor(private trackService: TrackService) {}
 
   ngAfterViewInit() {
-    this.width.set(this.childContainer.nativeElement.offsetHeight);
+    this.width.set(this.childContainer.nativeElement.offsetWidth);
+    this.height.set(this.childContainer.nativeElement.offsetHeight);
+  }
+
+  @HostListener("window:resize")
+  resize() {
+    this.width.set(this.childContainer.nativeElement.offsetWidth);
+    this.height.set(this.childContainer.nativeElement.offsetHeight);
   }
 }
